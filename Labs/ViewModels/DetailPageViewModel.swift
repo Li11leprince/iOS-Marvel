dcf5e2895a6b6fd029d889cbd3719f491a8442b5
Li11leprince <prosk300@gmail.com>
2023-04-20 23:36:51 +0600

    feat: minor changes

--- Labs/ViewModels/DetailPageViewModel.swift
+++ Labs/ViewModels/DetailPageViewModel.swift
@@ -10,26 +10,21 @@ import UIKit
 
 final class DetailPageViewModel {
     
-    private let api = APIManager()
+    private let rep = HeroRepositoryImp()
     private let db = DBManager()
     
     var isLoaded = false
     
     func getHero(id: Int, complition: @escaping (_ response: Swift.Result<HeroModel, Error>, _ isOffline: Bool) -> ()) {
         isLoaded = false
-        api.getCharacter(id: id) { [weak self] (response) in
+        rep.getCharacter(id: id) { [weak self] (response) in
             switch response {
             case .success(let hero):
                 self?.isLoaded = true
                 self?.db.saveHero(hero: hero)
                 complition(.success(hero), false)
             case .failure(let error):
-                guard let dbData = self?.db.getHero(by: id) else {
-                    complition(.failure(error), false)
-                    return
-                }
-                self?.isLoaded = true
-                complition(.success(dbData), true)
+                complition(.failure(error), false)
             }
         }
     }
--- Labs/ViewModels/HeroesViewModel.swift
+++ Labs/ViewModels/HeroesViewModel.swift
@@ -9,27 +9,20 @@ import Foundation
 
 final class HeroesViewModel {
     
-    private let api = APIManager()
+    private let rep = HeroRepositoryImp()
     private let db = DBManager()
-    
     var isLoaded = false
     
     func getHeroes(offset: Int, complition: @escaping (_ response: Swift.Result<[HeroListModel], Error>, _ isOffline: Bool) -> ()) {
         isLoaded = false
-        api.getAllCharacters(offset: offset) { [weak self] (response) in
+        rep.getAllCharacters(offset: offset) { [weak self] (response) in
             switch response {
             case .success(let heroes):
                 self?.isLoaded = true
                 self?.db.saveAllHeroes(heroes: heroes)
                 complition(.success(heroes), false)
             case .failure(let error):
-                let dbData = self?.db.getAllHeroes() ?? []
-                if dbData.isEmpty {
-                    complition(.failure(error), false)
-                } else {
-                    self?.isLoaded = true
-                    complition(.success(dbData), true)
-                }
+                complition(.failure(error), false)
             }
         }
     }
